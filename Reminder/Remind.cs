a04ed2fe5ffab280d9660ffbc23bea137bb7b957
Visocontor <yee@yeeeee.org>
2021-03-15 14:25:32 -0400

    Update 1.0.1

--- Reminder/Remind.cs
+++ Reminder/Remind.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Timers;
 using System.IO;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Reminder
 {
@@ -12,13 +13,71 @@ namespace Reminder
 
         public Remind()
         {
-            _timer = new Timer(1000) { AutoReset = true };
+            _timer = new Timer(5000) { AutoReset = true };
             _timer.Elapsed += TimerElapsed;
         }
 
         private void TimerElapsed(object sender, ElapsedEventArgs e)
         {
-            Console.Beep();
+            Random rand = new Random();
+
+            switch (rand.Next(1, 7))
+            {
+                case 1:
+                    new ToastContentBuilder()
+                        .AddArgument("action", "viewConversation")
+                        .AddArgument("conversationId", 9813)
+                        .AddText("Cunt")
+                        .AddText("Hey, you, you a bitch")
+                        .Show();
+                    break;
+
+                case 2:
+                    new ToastContentBuilder()
+                        .AddArgument("action", "viewConversation")
+                        .AddArgument("conversationId", 9813)
+                        .AddText("Hoe")
+                        .AddText("Go suck a dick, you whore")
+                        .Show();
+                    break;
+
+                case 3:
+                    new ToastContentBuilder()
+                        .AddArgument("action", "viewConversation")
+                        .AddArgument("conversationId", 9813)
+                        .AddText("I would love you...")
+                        .AddText("But you just make me sad.")
+                        .Show();
+                    break;
+
+                case 4:
+                    new ToastContentBuilder()
+                        .AddArgument("action", "viewConversation")
+                        .AddArgument("conversationId", 9813)
+                        .AddText("Worthless.")
+                        .AddText("That's what you are, WORTHLESS.")
+                        .Show();
+                    break;
+
+                case 5:
+                    new ToastContentBuilder()
+                        .AddArgument("action", "viewConversation")
+                        .AddArgument("conversationId", 9813)
+                        .AddText("Faggot")
+                        .AddText("Loser, I bet you kiss men, faggot.")
+                        .Show();
+                    break;
+
+                case 6:
+                    new ToastContentBuilder()
+                        .AddArgument("action", "viewConversation")
+                        .AddArgument("conversationId", 9813)
+                        .AddText("You weeb")
+                        .AddText("Stop watching the hentai you loser.")
+                        .Show();
+                    break;
+            }
+
         }
 
         public void Start()
