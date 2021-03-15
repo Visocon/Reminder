f75dc0300dd0b1c9648ef37168306a292569307e
Visocon <pochemo@protonmail.com>
2021-03-15 01:42:40 -0400

    Add project files.

---
+++ Reminder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Topshelf;
+
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Remind>(s =>
+                {
+                    s.ConstructUsing(remind => new Remind());
+                    s.WhenStarted(remind => remind.Start());
+                    s.WhenStopped(remind => remind.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("Reminder");
+                x.SetDisplayName("Reminder");
+                x.SetDescription("Puts you in your place.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ Reminder/Remind.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using System.IO;
+
+namespace Reminder
+{
+    public class Remind
+    {
+        private readonly Timer _timer;
+
+        public Remind()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.Beep();
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
